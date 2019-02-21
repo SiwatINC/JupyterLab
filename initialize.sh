454c6ff19401b433d04521d5faca8d7ced089e01
Siwat2545 <siwat2545@cud55.com>
2019-02-21 12:27:48 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -3,11 +3,12 @@ if [ -z "$(ls -A /config)" ]; then
    echo "Initializing . . ."
    jupyterhub --generate-config -f /config/jupyerhub_config.py
    echo "c.JupyterHub.config_file = 'jupyterhub_config.py'" >> /config/jupyterhub_config.py
+   echo $username:$password | chpasswd
    cd /config && jupyterhub --ip 0.0.0.0 --port $port --db='$db' --config='/config/jupyterhub_config.py'
-   adduser --home /config/$user $user
    exit 0
 else
    echo "Already Initiated, Starting JupyterLab"
+   echo $username:$password | chpasswd
    cd /config && jupyterhub --ip 0.0.0.0 --port $port --db='$db' --config='/config/jupyterhub_config.py'
    exit 0
 fi
