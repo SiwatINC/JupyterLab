a85fba33a05a3c191bd32bcacd77c0b7059093cc
Siwat2545 <siwat2545@cud55.com>
2019-02-22 11:35:06 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -5,12 +5,12 @@ if [ -z "$(ls -A /config)" ]; then
    mkdir -p /config/data
    cd /config && wget 'https://raw.githubusercontent.com/SiwatINC/JupyterLab/master/jupyerhub_config.py'
    echo admin:$password | chpasswd
-   cd /config && jupyterhub --ip 0.0.0.0 --port $port --config='/config/jupyterhub_config.py'
+   cd /config && jupyterhub --ip 0.0.0.0 --port $port --db=$db --config='/config/jupyterhub_config.py'
    exit 0
 else
    echo "Already Initiated, Starting JupyterLab"
    pip install ipyparallel
    echo admin:$password | chpasswd
-   cd /config && jupyterhub --ip 0.0.0.0 --port $port --config='/config/jupyterhub_config.py'
+   cd /config && jupyterhub --ip 0.0.0.0 --port $port --db=$db --config='/config/jupyterhub_config.py'
    exit 0
 fi
